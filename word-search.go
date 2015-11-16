455d3085faf5a39697297ae487df68b634a5681f
Breanne Boland <breanneboland@gmail.com>
2015-11-16 12:28:11 -0800

    word search now actually works, hey

--- word-search.go
+++ word-search.go
@@ -6,13 +6,13 @@ import (
 	)
 
 var wordList = []string {"cat", "abstentious", "arterious", "toaster", "placentious"}
-var letterList = []string {" a", "e", "i", "o", "u"} 
-//Added a space in front of the a to try to quick-and-dirty my way to a solution before going deeper into the problem.
 var letterMap = map[string]string{"a": "a", "e": "e", "i": "i", "o": "o", "u": "u"}
 //Could use index to cut looping short when one letter is in the wrong place
 //Could use length to determine quick True or False
 func main() {
-	fmt.Println(letterList)
+	letterList := []string {"", "a", "e", "i", "o", "u"} 
+	//First item is empty string because of how the slice initiates below
+
 	for _, word := range wordList {
 		vowelList := make([]string, 1)
 		for _, letter := range word {
@@ -20,9 +20,7 @@ func main() {
 				vowelList = append(vowelList, string(letter))
 			}
 		}
-		fmt.Println(vowelList)
-		fmt.Println(letterList)
-		//They appear to be the same, but output disagrees.
+
 		if reflect.DeepEqual(vowelList, letterList) {
 			fmt.Println(word, "has all the vowels in order, once each!")
 		} else {
